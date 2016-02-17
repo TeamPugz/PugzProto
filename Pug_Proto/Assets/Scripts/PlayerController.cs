70acb3835fcf29f935ad84351896185761a84ae3
Jared Ramey <rameyjared@gmail.com>
2016-02-17 15:56:54 -0800

    Player can move, started working on cats

---
+++ Pug_Proto/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerController : MonoBehaviour
+{ 
+    [HideInInspector]
+    public float Speed = 1;
+    [SerializeField]
+    private Rigidbody ThisBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        ThisBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerControls(Speed);
+    }
+
+    void PlayerControls(float player_speed)
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            ThisBody.transform.Translate(new Vector3(0, 0, Speed/2));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            ThisBody.transform.Rotate(new Vector3(0, -Speed*2, 0));
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            ThisBody.transform.Translate(new Vector3(0, 0, -Speed/2));
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            ThisBody.transform.Rotate(new Vector3(0, Speed*2, 0));
+        }
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
