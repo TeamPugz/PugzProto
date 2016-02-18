aa403e8d82ae714524c31bf043697b2de660ca66
Jared Ramey <rameyjared@gmail.com>
2016-02-17 17:55:49 -0800

    Worked on the cat, almost done

--- Pug_Proto/Assets/Scripts/CatController.cs
+++ Pug_Proto/Assets/Scripts/CatController.cs
@@ -1,29 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class CatController : MonoBehaviour
 {
-    [SerializeField]
     public GameObject Pos1;
-    [SerializeField]
     public GameObject Pos2;
-    [SerializeField]
     public GameObject Pos3;
-    [SerializeField]
     public GameObject Pos4;
 
     [SerializeField]
+    public float speed;
+    [SerializeField]
+    private GameObject[] TargetPos;
+    [SerializeField]
     private Rigidbody ThisBody;
+    [SerializeField]
+    private int CurrentPos = 1;
 
     // Use this for initialization
     void Start()
     {
-
+        ThisBody = GetComponent<Rigidbody>();
+        TargetPos = new GameObject[] { Pos1, Pos2, Pos3, Pos4};
+        Debug.Log(TargetPos.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
+        CatMovement(speed);
+    }
 
+    void CatMovement(float Cat_Speed)
+    {
+
+        if(ThisBody.transform.position != TargetPos[CurrentPos].transform.position)
+        {
+            //X
+            if(ThisBody.transform.position.x > TargetPos[CurrentPos].transform.position.x)
+            {
+                ThisBody.transform.Translate(new Vector3(-Cat_Speed, 0, 0));
+            }
+                else if(ThisBody.transform.position.x < TargetPos[CurrentPos].transform.position.x)
+                {
+                    ThisBody.transform.Translate(new Vector3(Cat_Speed, 0, 0));
+                }
+
+            //Z
+            if (ThisBody.transform.position.z > TargetPos[CurrentPos].transform.position.z)
+            {
+                ThisBody.transform.Translate(new Vector3(0, 0, -Cat_Speed));
+            }
+            else if (ThisBody.transform.position.z < TargetPos[CurrentPos].transform.position.z)
+            {
+                ThisBody.transform.Translate(new Vector3(Cat_Speed, 0, 0));
+            }
+        }
     }
 }
